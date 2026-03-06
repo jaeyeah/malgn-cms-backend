36d0d19b0097d3dcaeed5c84940197364611f189
jaeyeah <limjh819@naver.com>
2026-03-06 17:16:46 +0900

    Dao(Repository) 설정

---
+++ simple-cms-api/src/main/java/com/malgn/repository/ContentRepository.java
@@ -0,0 +1,9 @@
+package com.malgn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.malgn.entity.Content;
+
+public interface ContentRepository extends JpaRepository<Content, Long>{
+
+}
